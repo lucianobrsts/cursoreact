6aed131f901adb64b0e26376bf26a164bfd658b3
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-02 09:06:07 -0300

    Trabalhando com hooks

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,8 +1,10 @@
+import onePiece from '../assets/f5.jpeg'
 
 const Images = () => {
     return (
         <div>
             <img src="/f12.jpg" />
+            <img src={onePiece} />
         </div>
     )
 }
