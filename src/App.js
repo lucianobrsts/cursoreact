1970e10e3bb44eca501b06f194a74b4d7d991ad0
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-11-02 09:31:07 -0300

    trabalhando com useState e useEffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 import AnotherComponent from './components/AnotherComponent';
 import FirstComponent from './components/FirstComponent';
-import Images from './components/Images'
+import Hooks from './components/Hooks';
+import Images from './components/Images';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <FirstComponent />
       <AnotherComponent />
       <Images />
+      <Hooks />
     </div>
   );
 }
---
+++ src/components/Hooks.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+
+const Hooks = () => {
+    let idade = 30
+    const [novaIdade, setNovaIdade] = useState(40)
+
+    const changeAge = () => {
+        idade = 31
+    }
+
+    const changeNewAge = () => {
+        setNovaIdade(45)
+    }
+
+    useEffect(() => {
+        console.log('Testando')
+    })
+
+    return (
+        <div>
+            <p>Idade: {idade}</p>
+            <button onClick={changeAge}>Mudar idade</button>
+            <p>Idade: {novaIdade}</p>
+            <button onClick={changeNewAge}>Mudar nova idade</button>
+        </div>
+    )
+}
+
+export default Hooks
